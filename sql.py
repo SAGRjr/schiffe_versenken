17872c8f0b0f7e402ab8e8ade5e9100b8736230b
SAGRjr <andreaschaidaridis@gmail.com>
2023-05-23 23:09:30 +0200

    ein paar aenderungen

--- sql.py
+++ sql.py
@@ -7,10 +7,20 @@ try:
     connection = psycopg2.connect(host = "horton.db.elephantsql.com", user = "gmldbqwd", 
                                   password = "n4BFSabvT1NE-0fvDKjFD27lea1Z2uBU", dbname = "gmldbqwd")
     my_cursor = connection.cursor()
+
+    tabelle = """CREATE TABLE IF NOT EXIST Klassenkamerad(
+                        id          int PRIMARY KEY,
+                        Name        varchar(40) NOT NULL,
+                        Nachname    varchar(40) NOT NULL,
+                        Jahrgang    int)"""
+
+    connection.commit()
+
 except:
     print("An exception occurred")                              
 print(str(connection))
 print(str(my_cursor))
+
 if my_cursor:
-    my_cursor.execute("SELECT * FROM XXX")
+    my_cursor.execute(tabelle)
     connection.close()
