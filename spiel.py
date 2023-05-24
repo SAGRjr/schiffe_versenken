b0dc862234df0e935b194e3fbff34d0ce67b6f5d
DestyTM <Destyniki@gmail.com>
2023-05-24 14:22:10 +0200

    versuch3

--- spiel.py
+++ spiel.py
@@ -1,20 +1,60 @@
 import psycopg2
 
-connection = None
-my_cursor = None
 
-try:
-    connection = psycopg2.connect(host = "horton.db.elephantsql.com", user = "gmldbqwd", 
+
+def getConnection():
+    connection = None
+    my_cursor = None
+    try:
+        connection = psycopg2.connect(host = "horton.db.elephantsql.com", user = "gmldbqwd", 
                                   password = "n4BFSabvT1NE-0fvDKjFD27lea1Z2uBU", dbname = "gmldbqwd")
-except:
-    print("An exception occurred")                              
+    except:
+        print("An exception occurred")                              
 
+    my_cursor = connection.cursor()
+    return connection, my_cursor
 
-if my_cursor:
-    #ask user to put his login name
-    #ask user to put his password
-    #select user and password from Klassenkamerad ad check if there: select count(*) from  Klassenkamerad where Password='' and ....
+    #select user and password from Klassenkamerad and check if there: select count(*) from  Klassenkamerad where Password='' and ....
+def check_table_contents(result):
+
+    #my_cursor = connection.cursor
+    #my_cursor.execute("SELECT COUNT(*) FROM your_table")
+    #result = my_cursor.fetchone()
+    bLoginOK = False
     # if count(*) > 0 --> user can go on - else exit the program...
+    if result[0] > 0:
+        print("Eintrag gefunden.")
+        bLoginOK = True
+    else:
+        bLoginOK = False
+        print("Kein Eintrag vorhanden.")
 
-    # ask user if he wants to register new word or to play!
-    # 
+    # wenn result >0... dann bLoginOK = True
+ 
+    # ask user if he wants to register new word or to play! 
+    return bLoginOK
+
+
+def main():
+    # get connection to the DB
+    connection, my_cursor = getConnection()
+    if my_cursor:
+        login_data = input("Enter your username: ")
+         #ask user to put his password
+        passwd_entr = input("Enter your password: ")
+
+        sSelect = "SELECT COUNT(*) from Klassenkamerad where Login='" + login_data + "' and Password='" + passwd_entr + "'" 
+        print(sSelect)
+        my_cursor.execute(sSelect)
+        result = my_cursor.fetchone()
+        bLoginOK = check_table_contents(result)
+        print(str(result))
+        if bLoginOK == True:
+            input_option = input("Do you want to register (1) or play (2)?")
+
+
+    else:
+        print("cursor is None")
+    
+if __name__ == "__main__":
+    main()
